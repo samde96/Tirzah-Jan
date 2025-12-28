9ec1d614ca359da59b72a1b88677aa2c1c6da286
samdev614 <creativeaglemedia@gmail.com>
2025-12-28 19:08:40 +0300

    Updating NextJs

--- server/src/index.ts
+++ server/src/index.ts
@@ -49,5 +49,5 @@ app.use((err: any, req: express.Request, res: express.Response, next: express.Ne
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on port:${PORT}`);
 });
